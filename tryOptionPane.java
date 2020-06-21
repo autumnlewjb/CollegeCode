c27cff393a543513bc247792efaaacd3a168b367
Lew <autumnlewjb@gmail.com>
2020-06-21 15:31:40 +0800

    cleaning and tidying

--- tryOptionPane.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.jOptionPane;
-
-public class tryOptionPane{
-  public static void main(String[] args){
-    int age = jOptionPane.showInputDialog("Enter your age: ");
-    System.out.println(age);
-  }
-
-}
